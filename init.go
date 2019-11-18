adaedcb9e0e97ed81dd6c74719ab7b6f865f89d3
Stanislav Fedii <s.fedii@dailymotion.com>
2019-11-18 14:43:28 -0500

    Implemented environment file support.

--- init.go
+++ init.go
@@ -1,7 +1,12 @@
 package allure
 
 import (
+	"fmt"
 	"github.com/jtolds/gls"
+	"io"
+	"log"
+	"os"
+	"strings"
 )
 
 var (
@@ -13,9 +18,51 @@ var (
 const (
 	resultsPathEnvKey = "ALLURE_RESULTS_PATH"
 	wsPathEnvKey      = "ALLURE_WORKSPACE_PATH"
+	envFileKey        = "ALLURE_ENVIRONMENT_FILE_PATH"
 	nodeKey           = "current_step_container"
 )
 
 func init() {
 	ctxMgr = gls.NewContextManager()
+	if envFilePath := os.Getenv(envFileKey); envFilePath != "" {
+		envFilesStrings := strings.Split(envFilePath, "/")
+		resultsPath := os.Getenv(resultsPathEnvKey)
+		if _, err := copy(envFilePath, resultsPath+"/allure-results/"+envFilesStrings[len(envFilesStrings)-1]); err != nil {
+			log.Fatalf("Could not copy the environment file, %f", err)
+		}
+	}
+}
+
+func copy(src, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer func() {
+		if err = source.Close(); err != nil {
+			log.Fatalf("Could not close the stream for the environment file, %f", err)
+		}
+	}()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer func() {
+		if err = destination.Close(); err != nil {
+			log.Fatalf("Could not close the stream for the destination of the environment file, %f", err)
+		}
+	}()
+
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
 }
