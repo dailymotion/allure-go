99d57c8d71c43b8fb22e108a1c98520793022067
Stanislav Fedii <stanislav.fedii@gmail.coml>
2020-03-17 10:42:52 -0400

    Implemented a skip test function to mark tests as skipped.

--- example/example_skip_test.go
+++ example/example_skip_test.go
@@ -5,8 +5,16 @@ import (
 	"testing"
 )
 
-func TestSkip(t *testing.T) {
+func TestSkipStep(t *testing.T) {
 	allure.Test(t,
+		allure.Description("Skip test"),
+		allure.Action(func() {
+			allure.SkipStep(allure.Description("Skip"), allure.Action(func() {}), allure.Reason("reason"))
+		}))
+}
+
+func TestSkipTest(t *testing.T) {
+	allure.SkipTest(t,
 		allure.Description("Skip test"),
 		allure.Action(func() {
 			allure.SkipStep(allure.Description("Skip"), allure.Action(func() {}), allure.Reason("reason"))
